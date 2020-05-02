f2f337b92943097399aec9128e5eb235d336c8a5
Joel James <jayfiled@gmail.com>
2020-05-03 09:23:18 +1000

    Build Items component to house Item

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,11 @@
 // pages you need to route to and from go in the pages dir
 // next.js takes care of importing react, so no import needed here 😍
+import Items from '../components/Items';
 
 import Link from 'next/link'; // <- next.js way of routing without refreshing the page
 const Home = props => (
   <div>
-    <p>Home!</p>
+    <Items />
   </div>
 );
 export default Home;
