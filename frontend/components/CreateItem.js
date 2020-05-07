3a7e30ae3c122274abd182041beda0d4804a8f4a
Joel James <jayfiled@gmail.com>
2020-05-08 07:47:56 +1000

    add create form an link to state

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,18 +1,47 @@
-import React, { Component } from 'react';
-import { Mutation } from 'react-apollo';
-import gql from 'graphql-tag';
-import Form from './styles/Form';
-import formatMoney from '../lib/formatMoney';
-
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import Form from "./styles/Form";
+import formatMoney from "../lib/formatMoney";
 
 class CreateItem extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  state = {
+    title: "Rubber band",
+    description: "Elasticity",
+    price: 1000,
+    image: "band.png",
+    largeImage: "lrgBand.png",
+  };
+
+  handleChange = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+    const { name, type, value } = e.target;
+    const val = type === "number" ? parseFloat(value) : value;
+    this.setState({ [name]: val });
+  };
+
+  render() {
+    const { title, price, description } = this.state;
+    return (
+      <Form>
+        <fieldset>
+          <label htmlFor="title">
+            Title
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Title"
+              required
+              value={title}
+              onChange={this.handleChange}
+            />
+          </label>
+        </fieldset>
+      </Form>
+    );
+  }
 }
 
 export default CreateItem;
